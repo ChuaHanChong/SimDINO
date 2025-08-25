4c658f3e21d3c384608b0adbe3caf572e6f9e01d
Han Chong <chuahanchong93@gmail.com>
2025-08-25 20:31:25 +0800

    Add more dataset (#4)

--- simdinov2/data/datasets/__init__.py
+++ simdinov2/data/datasets/__init__.py
@@ -5,5 +5,10 @@
 
 from .image_net import ImageNet
 from .image_net21k import ImageNet21k
-from .image_ship_id_extra import ImageShipID_Extra
 from .image_ship_id import ImageShipID
+from .image_ship_id_extra import ImageShipID_Extra
+from .image_ship_id_100i import ImageShipID_100I
+from .image_ship_id_500i import ImageShipID_500I
+from .image_ship_id_1000i import ImageShipID_1000I
+from .image_ship_id_5000i import ImageShipID_5000I
+from .image_ship_id_10000i import ImageShipID_10000I
